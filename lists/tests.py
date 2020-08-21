ecef06ba274dae630539af0695db8cab44aceab7
silin <SXL1046@student.bham.ac.uk>
2020-08-21 16:10:03 +0800

    funactional_test.py hugely changed, adding enter example to-do list entering and showing previous input content

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from .views import home_page
 
 # Create your tests here.
@@ -10,5 +12,13 @@ class MineralTest(TestCase):
 
         result = resolve('/')
         self.assertEqual(result.func, home_page)
+
+    def test_home_page_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
 
     
