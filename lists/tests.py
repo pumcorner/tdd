660fadcaf1f6b7500549baf1856a76d58cb4f9a7
silin <SXL1046@student.bham.ac.uk>
2020-08-21 14:37:57 +0800

    dummy view to home_page 08/21

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
 # Create your tests here.
 
 class MineralTest(TestCase):
 
-    def test_bad_math(self):
-        
-       self.assertTrue(1 == 2)
+    def test_root_url_resolve_to_home_page_view(self):
+
+        result = resolve('/')
+        self.assertEqual(result.func, home_page)
+
+    
