aece75238ea05f661fe57669ee2917c1d09bb258
silin <SXL1046@student.bham.ac.uk>
2020-08-25 16:44:47 +0800

    add one FT helper function to remove duplications

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    #helper function 01
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
        self.browser.get('http://localhost:8000')
        self.assertIn('To-Do', self.browser.title,'Failed to find To-Do')
@@ -27,18 +33,24 @@ class NewVisitorTest(unittest.TestCase):
 
        inputbox.send_keys(Keys.ENTER)
        time.sleep(2)
+       self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       inputbox.send_keys('Use peacock feathers to make a fly')
+       inputbox.send_keys(Keys.ENTER)
+       time.sleep(2)
 
     # watch out for find_element and find_elements, with 's' means returning more than one.
     # find_element raises execption if no matches found, find_elements may return []
+       self.check_for_row_in_list_table('1: Buy peacock feathers')
+       self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-       table = self.browser.find_element_by_id('id_list_table')
-       rows = table.find_elements_by_tag_name('tr')
        #self.assertTrue(
                # in python 3.6+ we could use f"str"to insert local variables in curly syntax
                #any(row.text == '1: Buy peacock frather' for row in rows),f"New to-do item did not show in table. Content were \n {table.text}"
 #       )
 
-       self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+       #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
        self.fail('Finish the test!')
 
 if __name__ == '__main__':
