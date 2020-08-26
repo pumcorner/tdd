81320de71437a1e19bcc5ce80c2e99e999dec6c5
silin <SXL1046@student.bham.ac.uk>
2020-08-26 15:52:36 +0800

    redierct after post,show all items

--- lists/tests.py
+++ lists/tests.py
@@ -13,8 +13,37 @@ class MineralTest(TestCase):
         def test_can_save_a_POST_request(self):
 
             response = self.client.post('/',data={'item_text':'A new list item'})
-            self.assertIn('A new list item', response.content.decode())
-            self.assertTemplateUsed(response,'home.html')
+
+            self.assertEqual(Item.objects.count(),1)
+
+            new_item = Item.objects.first()
+            self.assertEqual(new_item.text, 'A new list item')
+
+            # response.content.decode() get the item_text
+            # instead of compiring with text constants we compire a html template we are using
+            # response.status.code == 302 means the webpage has been redirected
+            # response.status.code == 200 means the webpage has been rendered 
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response['location'], '/')
+
+        def test_redirects_atfer_POST(self):
+            response = self.client.post('/',data={'item_text':'A new list item'})
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response['location'], '/')
+
+        def test_only_save_item_when_necessary(self):
+
+            self.client.get('/')
+            self.assertEqual(Item.objects.count(),0)
+
+        def test_displays_all_list_items(self):
+            Item.objects.create(text = 'item 1')
+            Item.objects.create(text = 'item 2')
+
+            response = self.client.get('/')
+
+            self.assertIn('item 1', response.content.decode())
+            self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Item
 
 # Create your views here.
 
@@ -11,4 +11,10 @@ def home_page(request):
     # We have a multi dict key error here if using request.POST['item_text']
     # I guess that's because the default empty key is None and we actually get a '' in test one
     # Still confusing needs to look into doc
-    return render(request,'home.html',{'new_item_text': request.POST.get('item_text',''),})
+    if request.method == 'POST':
+        # objects.create() equals item=Item() & item.save()
+        Item.objects.create(text = request.POST['item_text'])
+        return redirect('/')
+    
+    items = Item.objects.all()
+    return render(request, 'home.html',{'items': items})
