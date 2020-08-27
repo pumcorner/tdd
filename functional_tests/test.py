dbfc75dd1b31a9c261d070ec6c3d3a5788b9911b
silin_li <SXL1046@student.bham.ac.uk>
2020-08-27 10:04:56 +0800

    Adding git remote repostory

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,10 @@
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -12,10 +14,21 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     #helper function 01
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        # if we get through codes we return and jump out of loop
+        # if we caught exception in brackets we sleep 0.5s and retry by looping
+        # if we always loop we stuck that's when MAX_WAIT could work
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # in unit.test we use hard url, in LSTC we use attribute
@@ -33,18 +46,16 @@ class NewVisitorTest(LiveServerTestCase):
        inputbox.send_keys(' Buy peacock feathers')
 
        inputbox.send_keys(Keys.ENTER)
-       time.sleep(2)
-       self.check_for_row_in_list_table('1: Buy peacock feathers')
+       self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
        inputbox = self.browser.find_element_by_id('id_new_item')
        inputbox.send_keys('Use peacock feathers to make a fly')
        inputbox.send_keys(Keys.ENTER)
-       time.sleep(2)
 
     # watch out for find_element and find_elements, with 's' means returning more than one.
     # find_element raises execption if no matches found, find_elements may return []
-       self.check_for_row_in_list_table('1: Buy peacock feathers')
-       self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+       self.wait_for_row_in_list_table('1: Buy peacock feathers')
+       self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
        #self.assertTrue(
                # in python 3.6+ we could use f"str"to insert local variables in curly syntax
